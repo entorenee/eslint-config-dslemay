0c96f012b2bea1e2eadb7d2749756bb95fde953f
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-04-18 21:00:22 -0700

    feat: add eslint base ruleset (#1)
    * feat: add best practices rules
    
    * feat: add possible errors rules
    
    * feat: add script to check base ESLint rules
    
    * fix: update rules which may conflict with Prettier
    
    * feat: install eslint config prettier
    
    * feat: add stylistic rule definitions
    
    * fix: correct rule name typos
    
    * feat: install babel-eslint
    
    * feat: add es6 rule set

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    './possible-errors.js',
+    './best-practices.js',
+    './stylistic.js',
+    './es6.js',
+  ],
+};
