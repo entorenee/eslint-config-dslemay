fc65039fede9a3c1a28f1163b4bbe38e33c30a88
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-07-29 18:34:07 -0700

    feat(rules): add jest/no-export rule
    BREAKING CHANGE: The jest/no-export rule is new. It may cause
    new errors for existing codebases

--- jest.js
+++ jest.js
@@ -15,6 +15,7 @@ module.exports = {
     'jest/no-disabled-tests': 'warn',
     'jest/no-duplicate-hooks': 'error',
     'jest/no-empty-title': 'error',
+    'jest/no-export': 'error',
     'jest/no-focused-tests': 'error',
     'jest/no-hooks': 'off',
     'jest/no-identical-title': 'error',
