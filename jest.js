51113a4d103485ccf54ab26e32cd6901c8831261
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2020-01-09 09:28:40 -0800

    fix(rules): remove defunct jest/no-empty-title rule

--- jest.js
+++ jest.js
@@ -14,7 +14,6 @@ module.exports = {
     'jest/no-commented-out-tests': 'warn',
     'jest/no-disabled-tests': 'warn',
     'jest/no-duplicate-hooks': 'error',
-    'jest/no-empty-title': 'error',
     'jest/no-expect-resolves': 'off', // need more real world use to determine
     'jest/no-export': 'error',
     'jest/no-focused-tests': 'error',
